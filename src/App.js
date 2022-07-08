123e64894c0c2df99ad9c04f317cdec77432b314
Nika Gabriadze <67961868+nickgabriadze@users.noreply.github.com>
2022-07-08 11:46:51 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,127 +1,170 @@
 import React from 'react';
 import { useReducer } from 'react';
 import './App.css';
-import {DelButton, DigitButton, Operators, OtherButtons,EqualsButton} from './buttons.js'
+import { DelButton, DigitButton, Operators, OtherButtons, EqualsButton } from './buttons.js'
 
 
 export const ACTIONS = {
-   DISPLAY_DIGIT: "DISPLAY_DIGIT",
-   DISPLAY_OPERATION: "DISPLAY_OPERATION",
-   EVALUATE: "EVALUATE",
-   ALL_CLEAR: "ALL_CLEAR",
-   DELETE: "DEL"
+  DISPLAY_DIGIT: "DISPLAY_DIGIT",
+  DISPLAY_OPERATION: "DISPLAY_OPERATION",
+  EVALUATE: "EVALUATE",
+  ALL_CLEAR: "ALL_CLEAR",
+  DELETE: "DEL"
 };
 
-const reducer = (state, {type, payload}) => {
-  const evaluated = false
-  switch(type){
+const reducer = (state, { type, payload }) => {
 
+  switch (type) {
     case ACTIONS.DISPLAY_DIGIT:
-      if(state.expr == null){
-        state.expr = ""
+      if (state.expr == null & state.main == null) {
+        state.expr = ''
+        state.main = ''
+      }
+      if (state.expr.length > state.main.length) {
+        state.main = state.expr
+      }
+      if (state.expr[0] == 0 & payload.digit == '0') {
+        return state;
+      }
+      if (state.expr[0] == 0 & state.expr >= 1) {
+        state.expr = state.expr.slice(1,)
+        state.main = state.expr
       }
 
-      if(state.evaluated == true){
-        
-        return {...state, expr: '' + payload.digit,
-        evaluated: false};
-      }
-      
-      if(state.expr.includes(".") & payload.digit === "."){
+      if (state.expr.includes(".") & payload.digit === '.') {
         return state;
       }
-      
-    
-      return({
-        ...state,
-        expr: state.expr + payload.digit,
-        operation: false,
-        digitDisplayed: true
-      })
-      
-    case ACTIONS.DISPLAY_OPERATION:      
-  
-      if(state.expr == null & payload.operation == "-"){
-        state.expr = ''
-        return({
+      if (state.evaluated) {
+        return ({
           ...state,
-        expr: state.expr  + payload.operation,
-        operation: true,
-        digitDisplayed: false
+          expr: '',
+          main: '',
+          evaluated: false
         })
-      }
-      if(state.expr == null){
-        return state;
-      }
-      if(state.operation == true){
-        return state;
       }
       return ({
         ...state,
-        expr: state.expr + payload.operation,
-        operation: true,
-        digitDisplayed: false
+        expr: state.expr + payload.digit,
+        main: state.main + payload.digit,
+        opCount: false
       })
 
-    case ACTIONS.ALL_CLEAR:
-      if(state.expr != null){
-        return({...state, expr: ""})
-        
+    case ACTIONS.DISPLAY_OPERATION:
+
+      if ((state.expr == null || state.expr == '') & payload.operation == '-') {
+        state.expr = ''
+        state.main = ''
+        return ({
+          ...state,
+          expr: state.expr + '-',
+          main: state.expr + '-'
+
+        })
       }
 
-    case ACTIONS.EVALUATE:
-      if(state.digitDisplayed == false){
+      if (state.expr == null || state.expr == '') {
         return state;
       }
-     
-      if(state.expr != null){
-        return({...state, expr: eval(state.expr), 
-        evaluated: true})
-      
-    }
+
+      if (state.opCount == true) {
+        return state;
+      }
+
+      return ({
+        ...state,
+        main: state.main + payload.operation,
+        expr: '',
+        opCount: true
+      })
 
     case ACTIONS.DELETE:
-      if(state.expr == null){
+      if (state.expr == null & state.main == null) {
         return state;
       }
-      
-      return({
+
+      if (state.expr === '') {
+        state.expr = state.main
+        state.main = ''
+      }
+
+      return (
+        {
+          ...state,
+          expr: state.expr.slice(0, -1),
+        }
+      )
+    case ACTIONS.ALL_CLEAR:
+      if (state.expr == null || state.main == null) {
+        return state;
+      }
+
+      return ({
         ...state,
-        expr: state.expr.toString().slice(0, -1)
+        expr: '',
+        main: '',
+        opCount: false
       })
-    }
+    case ACTIONS.EVALUATE:
+      if (state.main == null || state.main == '') {
+        return state;
+      }
+      const lastOne = state.main[state.main.length - 1];
 
-  
+      if (lastOne == '/' ||
+        lastOne == '*' ||
+        lastOne == '-' ||
+        lastOne == '+') {
+        state.main = state.main.slice(0, -1)
+      }
 
+      if (state.expr == '.') {
+        return ({
+          main: '0',
+          expr: '',
+          evaluated: true
+        })
+      }
+
+      return ({
+        main: eval(state.main).toString(),
+        expr: '',
+        evaluated: true
+
+      })
+  }
 
 }
+
+
 function Calculator() {
-   const [{expr, evaluated, operation, digitDisplayed}, dispatch] = useReducer(reducer, {});
-   
+  const [{ expr, main, opCount, evaluated }, dispatch] = useReducer(reducer, {});
+
   return (
     <div className="calculator">
-    <div id="output">
-    <div id="first">{expr}</div>
-    </div>
-    <OtherButtons  className="others" AC="AC" dispatch={dispatch}/>
-    <DelButton className="others" DEL="DEL" dispatch={dispatch}/>
-    <Operators operation={"+"} dispatch={dispatch}/>
-    <Operators operation={"-"} dispatch={dispatch}/>
-    <Operators operation={"/"} dispatch={dispatch}/>
-    <Operators operation={"*"} dispatch={dispatch}/>
-    <DigitButton digit={"1"} dispatch={dispatch}/>
-    <DigitButton digit={"2"} dispatch={dispatch}/>
-    <DigitButton digit={"3"} dispatch={dispatch}/>
-    <DigitButton digit={"4"} dispatch={dispatch}/>
-    <DigitButton digit={"5"} dispatch={dispatch}/>
-    <DigitButton digit={"6"} dispatch={dispatch}/>
-    <DigitButton digit={"7"} dispatch={dispatch}/>
-    <DigitButton digit={"8"} dispatch={dispatch}/>
-    <DigitButton digit={"9"} dispatch={dispatch}/>
-    <DigitButton digit={"0"} dispatch={dispatch}/>
-    <DigitButton digit={"."} dispatch={dispatch}/>
-    <EqualsButton dispatch={dispatch}/>
-    
+      <div id="output">
+        <div id="second">{main}</div>
+        <div id="first">{expr}</div>
+
+      </div>
+      <OtherButtons className="others" AC="AC" dispatch={dispatch} />
+      <DelButton className="others" DEL="DEL" dispatch={dispatch} />
+      <Operators operation={"+"} dispatch={dispatch} />
+      <Operators operation={"-"} dispatch={dispatch} />
+      <Operators operation={"/"} dispatch={dispatch} />
+      <Operators operation={"*"} dispatch={dispatch} />
+      <DigitButton digit={"1"} dispatch={dispatch} />
+      <DigitButton digit={"2"} dispatch={dispatch} />
+      <DigitButton digit={"3"} dispatch={dispatch} />
+      <DigitButton digit={"4"} dispatch={dispatch} />
+      <DigitButton digit={"5"} dispatch={dispatch} />
+      <DigitButton digit={"6"} dispatch={dispatch} />
+      <DigitButton digit={"7"} dispatch={dispatch} />
+      <DigitButton digit={"8"} dispatch={dispatch} />
+      <DigitButton digit={"9"} dispatch={dispatch} />
+      <DigitButton digit={"0"} dispatch={dispatch} />
+      <DigitButton digit={"."} dispatch={dispatch} />
+      <EqualsButton dispatch={dispatch} />
+
     </div>
   );
 }
